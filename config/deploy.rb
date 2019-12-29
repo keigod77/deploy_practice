0111a7fcc1e0ab0c701343bda717c87b06945353
Keigo Dohmae <keigod0707@gmail.com>
2019-12-29 19:01:30 +0900

    capistrano導入

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+lock '3.11.2'
+
+set :application, 'deploy_practice'
+
+set :repo_url,  'git@github.com:keigod77/deploy_practice.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.5.1'
+
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/deploy_key.pem'] 
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
---
+++ config/deploy/production.rb
@@ -0,0 +1,1 @@
+server '18.177.83.253', user: 'ec2-user', roles: %w{app db web}
